f9e9801dc49966c1aec9df65f1947cdb27ced7cc
Dan Griffith <dan.griffith@tierpoint.com>
2018-04-17 09:53:55 -0500

    add files

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Javascript is Fun', pages: 101},
+        {title: 'Learn to Draw', pages: 23},
+        {title: 'Stabbing Clowns', pages: 131},
+        {title: 'React, Redux, Ridiculous!', pages: 535},
+    ]
+}
+
